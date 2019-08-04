272266fce91f0821425f7d3ed850886a11e452cc
Ollie Haydon-Mulligan <ollie.bhm@gmail.com>
2019-08-04 20:39:52 +0100

    Select mention when using arrow keys

--- app/webpacker/controllers/text_editor_controller.js
+++ app/webpacker/controllers/text_editor_controller.js
@@ -3,10 +3,40 @@ import { Controller } from "stimulus"
 export default class extends Controller {
 	static targets = ["editor"]
 
+	connect(){
+		this.element.addEventListener("keydown", (event)=>{
+			this.selection = document.getSelection()
+			// TO DO: This isn't working when pressing right arrow to select mention that isn't at beginning of text area
+			if (this.selection.anchorNode.parentElement.dataset.mention) {
+				if (this._isSelectingFromEnd(event) || this._isSelectingFromBeginning(event)){
+					event.preventDefault()
+					this._selectElement(this.selection.anchorNode)
+				}
+			}
+		})
+	}
+
 	insertMention(){
 		this._insertAtStart(`<span class="mention" data-mention="123">@test mention</span>&nbsp`);
 		this._setCursorToEnd();
 		this.editorTarget.focus();
+	}
+
+	_isSelectingFromEnd(event){
+		return (event.keyCode === 37 || event.keyCode === 8) && this.selection.anchorOffset == this.selection.anchorNode.length
+	}
+
+	_isSelectingFromBeginning(event){
+		return event.keyCode === 39 && this.selection.anchorOffset == 0 && this.selection.isCollapsed
+	}
+
+	_selectElement(element){
+		var range = document.createRange()
+		range.setStart(element, 0)
+		range.setEnd(element, element.length)
+		var selection = document.getSelection()
+		selection.removeAllRanges()
+		selection.addRange(range)
 	}
 
 	_insertAtStart(html){
@@ -15,7 +45,6 @@ export default class extends Controller {
 		range.collapse(true);
 		var fragment = this._createFragment(html);
 		range.insertNode(fragment);
-		return range;
 	}
 
 	_createFragment(html){
@@ -23,7 +52,7 @@ export default class extends Controller {
     el.innerHTML = html;
     var frag = document.createDocumentFragment(), node, lastNode;
     while ( (node = el.firstChild) ) {
-        lastNode = frag.appendChild(node);
+      lastNode = frag.appendChild(node);
     }
     var firstNode = frag.firstChild;
     return frag;
